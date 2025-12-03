7dd5b2fe5403afc318e9ef73c15e309563a9ba69
Elodie Daigle <elodiedaigle@gmail.com>
2025-12-03 08:48:28 -0500

    Routes : Ajout des routes nécessaires au login

--- app/routes/web.php
+++ app/routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', 'HomeController@index');
 // Page d'inscription
 Route::get('/register', 'AuthController@register');
 Route::post('/register', 'AuthController@registerPost');
+
+// Page de login
+Route::get('/login', 'AuthController@login');
+Route::post('/login', 'AuthController@loginPost');
