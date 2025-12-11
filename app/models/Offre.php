c795cb7e49bacf447f008dbc7b19736aeec8623b
Elodie Daigle <elodiedaigle@gmail.com>
2025-12-11 17:05:52 -0500

    Formulaire d'offres : Création du formulaire, ajustements, validations, CSS et responsive

---
+++ app/models/Offre.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Models;
+
+class Offre extends CRUD {
+    protected $table = 'offre';
+    protected $primaryKey = 'idoffre';
+    protected $fillable = [
+        'montant_offert',
+        'date_offre',
+        'utilisateur_idutilisateur',
+        'enchere_idenchere'
+    ];
+
+    public function getByEnchere($idEnchere) {
+        $sql = "SELECT o.*, u.prenom, u.nom
+                FROM offre o
+                INNER JOIN utilisateur u 
+                    ON u.idutilisateur = o.utilisateur_idutilisateur
+                WHERE o.enchere_idenchere = ?
+                ORDER BY o.montant_offert DESC";
+
+        $stmt = $this->prepare($sql);
+        $stmt->execute([$idEnchere]);
+        return $stmt->fetchAll();
+    }
+
+    public function getHighestBid($idEnchere) {
+        $sql = "SELECT MAX(montant_offert) AS max FROM offre WHERE enchere_idenchere = ?";
+        $stmt = $this->prepare($sql);
+        $stmt->execute([$idEnchere]);
+        return $stmt->fetch()['max'] ?? null;
+    }
+}
